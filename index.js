ceb03b246174044277285bfd6c51876e8e8abd3f
David Lee <david.lee@census.gov>
2016-02-11 21:13:38 -0500

    new airtable code

--- index.js
+++ index.js
@@ -18,3 +18,58 @@ app.listen(app.get('port'), function() {
 });
 
 
+
+//CODE
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: 'keyWInwqgSshQe7GV' }).base('app4ilATYQGuMBgjp');
+
+app.get('/herokuTest', function (request, response) {
+	request.send('Hello World!');
+});
+app.get('/airtableTest', function (request, response) {
+	base('Route Data').select({
+    // Selecting the first 3 records in Main View:
+      maxRecords: 3,
+      view: "Main View"
+    }).eachPage(function page(records, fetchNextPage) {
+
+      // This function (`page`) will get called for each page of records.
+
+      records.forEach(function(record) {
+        console.log('Retrieved ', record.get('Auto Number'));
+        response.end('Hello World\n');
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+
+    }, function done(error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+	//request.send('Hello World!');
+});
+
+
+
+
+//OUTPUT
+//Retrieved 1
+
+//FETCH FIRST PAGE
+// If you only want the first page of records, you can
+// use `firstPage` instead of `eachPage`.
+/*base('Route Data').select({
+    view: 'Main View'
+}).firstPage(function(error, records) {
+    if (error) {
+        console.log(error);
+    } else {
+        records.forEach(function(record) {
+            console.log('Retrieved ', record.get('Auto Number'));
+        });
+    }
+});*/
