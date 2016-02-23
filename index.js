d7ce5c1f6b71165edea1292bc74540f29938c961
David Lee <david.lee@census.gov>
2016-02-23 14:26:34 -0500

    removed some typos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
-np
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
