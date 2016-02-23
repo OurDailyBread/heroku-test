03999dc07263e74834ad3ccf325167bedc00eb6b
David Lee <david.lee@census.gov>
2016-02-23 14:15:45 -0500

    added cross origin response headers

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 //app.configure(function() {
 //  app.use(bodyParser.urlencoded({ extended: false });
 //  app.use(bodyParser.json());
