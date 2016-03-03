db295d6146969633508e318bf5e5d3ceccfdd82d
David Lee <david.lee@census.gov>
2016-03-03 11:56:33 -0500

    update postJSON to nested addresses

--- index.js
+++ index.js
@@ -148,7 +148,8 @@ app.post('/markVacant', function(request, response) {
 
   console.log('POST received ' + JSON.stringify(request.body));
 
-  var postJSON = JSON.parse(request.body.addresses.addresses);
+  var postJSON = JSON.parse(request.body.addresses);
+  postJSON = postJSON.addresses;
   console.log(JSON.stringify(postJSON));
   for (var index in postJSON.addresses) {
     console.log('processing ' + postJSON.addresses[index].address);
